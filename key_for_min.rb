f0d7942ba685b69b15df310098753d1b4974566b
Winston  Chung <18.chung.winston@ctkprep.org>
2019-07-24 20:22:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  mine = name_hash.first
-  for i in name_hash
-    if i < mine
+  mine = 0
+  name_hash.each do |key,int|
+    if mine == 0
+      mine = int
+    elsif i < mine
     mine = i
     end
   end
